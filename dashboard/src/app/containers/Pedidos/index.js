a2c07bfbd6aa73e933b71f578ad3fc755108aa93
Bruno <brunodelacqua@gmail.com>
2020-09-01 11:42:12 -0300

    Pedidos

--- dashboard/src/app/containers/Pedidos/index.js
+++ dashboard/src/app/containers/Pedidos/index.js
@@ -5,13 +5,16 @@ import Titulo from "../../components/Texto/Titulo";
 
 import Pesquisa from "../../components/Inputs/Pesquisa";
 import Tabela from "../../components/Tabela/Simples";
+import Paginacao from "../../components/Paginacao/Simples";
 
 class Pedidos extends Component {
   state = {
-    pesquisa: ""
+    pesquisa: "",
+    atual: 0,
   };
 
-  onChangePesquisa = ev => this.setState({ pesquisa: ev.target.value });
+  onChangePesquisa = (ev) => this.setState({ pesquisa: ev.target.value });
+  changeNumeroAtual = (atual) => this.setState({ atual });
 
   render() {
     const { pesquisa } = this.state;
@@ -23,22 +26,22 @@ class Pedidos extends Component {
         "Valor Total": 89.9,
         Data: moment().toISOString(),
         Situação: "Aguardando Pagamento",
-        botaoDetalhes: "/pedido/d6s7dd8d6s7dsa768"
+        botaoDetalhes: "/pedido/d6s7dd8d6s7dsa768",
       },
       {
         Cliente: "Cliente 2",
         "Valor Total": 189.9,
         Data: moment().toISOString(),
         Situação: "Aguardando Concluído",
-        botaoDetalhes: "/pedido/d6s78drr6s7dsa768"
+        botaoDetalhes: "/pedido/d6s78drr6s7dsa768",
       },
       {
         Cliente: "Cliente 3",
         "Valor Total": 8,
         Data: moment().toISOString(),
         Situação: "Aguardando Pagamento",
-        botaoDetalhes: "/pedido/d6s78dyy6s7dsa768"
-      }
+        botaoDetalhes: "/pedido/d6s78dyy6s7dsa768",
+      },
     ];
 
     return (
@@ -49,19 +52,22 @@ class Pedidos extends Component {
           <Pesquisa
             valor={pesquisa}
             placeholder={"Pesquisa aqui pelo nome do cliente..."}
-            onChange={ev => this.onChangePesquisa(ev)}
+            onChange={(ev) => this.onChangePesquisa(ev)}
+            onClick={() => alert("Pesquisar")}
           />
           <br />
           <Tabela
             cabecalho={["Cliente", "Valor Total", "Data", "Situação"]}
             dados={dados}
           />
-          {/* <Paginacao
-            atual={this.state.atual}
-            total={this.props.pedidos ? this.props.pedidos.total : 0}
-            limite={this.state.limit}
-            onClick={numeroAtual => this.changeNumeroAtual(numeroAtual)}
-          /> */}
+          {
+            <Paginacao
+              atual={this.state.atual}
+              total={120}
+              limite={20}
+              onClick={(numeroAtual) => this.changeNumeroAtual(numeroAtual)}
+            />
+          }
         </div>
       </div>
     );
